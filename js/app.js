f9843211362a84a60262da67ffea1609b3d730c4
Richard Hijzen <richardhijzen@hotmail.com>
2018-09-08 13:27:13 +0200

    added javascript to change form

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function logIn(){
+    document.getElementById('loginform').style.display ='block';
+    document.getElementById('signupform').style.display = 'none';
+  }
+  
+  function signUp(){
+    document.getElementById('loginform').style.display = 'none';
+    document.getElementById('signupform').style.display = 'block';
+  }
